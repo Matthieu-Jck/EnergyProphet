e012b6b9add9b1d5b608ecc69d92b4414d18398d
Matthieu Jacques <Matthieu.Jacques.2077@gmail.com>
2025-09-09 00:02:20 +0200

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:8080",
-                changeOrigin: true,
-            },
-        },
-    },
+  plugins: [react()],
+  base: '/EnergyProphet/',
 });
